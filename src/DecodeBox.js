4518cda2f4e3f3c84545b54a7f2dfed7ce60947f
Linawaty <linawatylim@gmail.com>
2020-07-11 19:05:40 +0800

    Decode Message completed

--- src/DecodeBox.js
+++ src/DecodeBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SKIP from './AppSetting.js';
 
 export default class DecodeBox extends Component {
     constructor(props) {
@@ -8,13 +9,26 @@ export default class DecodeBox extends Component {
         }
     }
 
+    decodeMsg = (msg, skip) => {
+        let decodedMsg = msg.split('').map(char => {
+            return String.fromCharCode(char.charCodeAt(0) - skip);
+        });
+
+        return decodedMsg.join('');
+    }
+
+    handleChange = (evnt) => {
+        this.setState({decMessage: this.decodeMsg(evnt.target.value, SKIP)});
+        console.log(this.decodeMsg(evnt.target.value, SKIP));
+    }
+
     render() {
         return (
             <div className="box dec">
                 <h2>DECODE</h2>
                 <h4>Message</h4>
-                <textarea rows="8" aria-multiline></textarea>
-                <h4>Encrypted Message</h4>
+                <textarea rows="8" aria-multiline autoFocus onKeyUp={(event) => this.handleChange(event)}></textarea>
+                <h4>Decrypted Message</h4>
                 <p>{this.state.decMessage}</p>
             </div>
         )
