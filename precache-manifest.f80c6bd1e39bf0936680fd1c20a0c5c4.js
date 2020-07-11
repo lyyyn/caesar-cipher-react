dad07cf1f993492adb4544837f73060fa8eb8c76
Linawaty <linawatylim@gmail.com>
2020-07-11 19:07:33 +0800

    Updates

--- precache-manifest.f80c6bd1e39bf0936680fd1c20a0c5c4.js
+++ precache-manifest.f80c6bd1e39bf0936680fd1c20a0c5c4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea28a27beab45b4d2b170d323e29cc52",
+    "revision": "4503e8125a05355d46f0d66ace01bb1a",
     "url": "/caesar-cipher-react/index.html"
   },
   {
-    "revision": "965411310b999ecd14c1",
-    "url": "/caesar-cipher-react/static/css/main.3cdcb115.chunk.css"
+    "revision": "8b050835f2ea567d7b6f",
+    "url": "/caesar-cipher-react/static/css/main.10533fcb.chunk.css"
   },
   {
     "revision": "7c509f5593588e0790e7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/caesar-cipher-react/static/js/2.8b0d8b28.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "965411310b999ecd14c1",
-    "url": "/caesar-cipher-react/static/js/main.d2558dc4.chunk.js"
+    "revision": "8b050835f2ea567d7b6f",
+    "url": "/caesar-cipher-react/static/js/main.c69d8517.chunk.js"
   },
   {
     "revision": "525ef8f968fa77323edd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caesar-cipher-react/precache-manifest.6d2b1accc8861b3f924041f92550cb2e.js"
+  "/caesar-cipher-react/precache-manifest.f80c6bd1e39bf0936680fd1c20a0c5c4.js"
 );
 
 self.addEventListener('message', (event) => {
